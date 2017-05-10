80eab7f2d18cf63c5b7afb24526a531e7ce40c33
ananjaser1211 <ananjaser@gmail.com>
2017-05-10 20:02:02 +0300

    Final Refined RC Update.
             Release version for Refined/RefinedLite v3.7
    
             -ReArrange RC
             -RC More virtual buttons
             -RC Gradiant Statusbar
             -RC Battert % Color
             -RC Custom Toast Animation
             -RC Custom Carrier Text on Notifications
             -Cleanups and some minor changes.
    
             6/15/2017

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -875,7 +875,7 @@ public final class R {
 		public static final int abc_toolbar_collapse_description = 0x7f090012;
 		public static final int appbar_scrolling_view_behavior = 0x7f090026;
 		public static final int bottom_sheet_behavior = 0x7f09002c;
-		public static final int character_counter_pattern = 0x7f09002f;
+		public static final int character_counter_pattern = 0x7f090030;
 		public static final int search_menu_title = 0x7f090013;
 		public static final int status_bar_notification_info_overflow = 0x7f090014;
 	}
